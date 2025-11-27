e27ac3d01b9ac74c287d98b4a6dad55edc523731
mukkerasaiteja <mukkerasaiteja@gmail.com>
2025-11-27 13:53:28 -0600

    Refractor code

--- backend/config.js
+++ backend/config.js
@@ -6,7 +6,7 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-dotenv.config({ path: path.resolve(__dirname, "../.env") });
+dotenv.config({ path: path.resolve(__dirname, "./.env") });
 
 const PORT_SERVER = process.env.PORT || 8080;
 const MONGO_URI = process.env.MONGO_URI;
