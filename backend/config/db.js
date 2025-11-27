e32f0d81afe3d853943a57666071ba1a8522fde3
mukkerasaiteja <mukkerasaiteja@gmail.com>
2025-11-27 13:46:26 -0600

    Refactor: separate backend and frontend

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,7 +6,7 @@ async function connectDB() {
     const conn = await mongoose.connect(MONGO_URI);
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (err) {
-    console.log("Error connecting with DB: " + err.message);
+    console.log("Error connecting with DB: " + err?.message);
     process.exit(1);
   }
 }
